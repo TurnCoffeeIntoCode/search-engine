a14519056916e6d804962288584e7705035cdfe9
Chris Hurst <46472228+ytsruh@users.noreply.github.com>
2024-04-14 15:08:57 +0100

    feat: added cache to search

--- routes/routes.go
+++ routes/routes.go
@@ -1,9 +1,12 @@
 package routes
 
 import (
+	"time"
+
 	"github.com/a-h/templ"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/adaptor"
+	"github.com/gofiber/fiber/v2/middleware/cache"
 )
 
 func render(c *fiber.Ctx, component templ.Component, options ...func(*templ.ComponentHandler)) error {
@@ -18,7 +21,16 @@ func SetRoutes(app *fiber.App) {
 	app.Get("/login", LoginHandler)
 	app.Post("/login", LoginPostHandler)
 	app.Post("/logout", LogoutHandler)
+
 	app.Post("/search", HandleSearch)
+	app.Use("/search", cache.New(cache.Config{
+		Next: func(c *fiber.Ctx) bool {
+			return c.Query("noCache") == "true"
+		},
+		Expiration:   30 * time.Minute,
+		CacheControl: true,
+	}))
+
 	// app.Get("/create", func(c *fiber.Ctx) error {
 	// 	u := &db.User{}
 	// 	u.CreateAdmin()
